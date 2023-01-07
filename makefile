671dc441b966269183d47732908aba9c804d57ed
TheDemonicStaff-t <81659118+TheDemonicStaff-t@users.noreply.github.com>
2023-01-07 04:49:33 +0000

    begun implementing fs

--- makefile
+++ makefile
@@ -1,10 +1,16 @@
-DIMG := $(wildcard *.iso)
+.PHONY: deps clean all mount umount bootloader kernel dimg
 
-.PHONY: deps clean all mount umount
+dimg: bootloader
+	dd if=/dev/zero of=/workspaces/DemonOs-Bios-Asm/DemonOs.iso bs=1024 count=102400
+	mkfs.ext2 DemonOs.iso
+	dd if=/workspaces/DemonOs-Bios-Asm/build/boot.bin of=/workspaces/DemonOs-Bios-Asm/DemonOs.iso conv=notrunc
+
+bootloader:
+	nasm -f bin src/bootloader/boot.s -o build/boot.bin
 
 mount:
 	sudo mkdir /mnt/dimg
-	sudo mount -t ext4 $(DIMG) /mnt/dimg
+	sudo mount -t ext2 DemonOs.iso /mnt/dimg
 
 umount:
 	sudo umount /mnt/dimg
@@ -14,3 +20,6 @@ deps:
 	sudo apt update -y
 	sudo apt upgrade -y
 	sudo apt install nasm -y
+
+clean:
+	rm build/boot.bin
--- src/bootloader/boot.s
+++ src/bootloader/boot.s
@@ -14,7 +14,52 @@ start:
 
 
 
-times 512 - ($-$$) db 0
-db 0xAA55
+times 510 - ($-$$) db 0
+dw 0xAA55
 
-pci_data:
+times 512 - ($-($$+0x200)) db 0
+ext_superblock_start:
+
+TOTAL_INODES:               dd 0x00006400 ; 
+TOTAL_BLOCKS:               dd 0x00006400
+SUPER_USER_RES_BLOCKS:      dd 0x00000500
+UNUSED_BLOCK_COUNT:         dd 0x000060D0
+UNUSED_INODE_COUNT:         dd 0x000063F5
+SUPERBLOCK_BLOCK_NUMBER:    dd 0x00000000
+ENCODED_BLOCK_SIZE:         dd 0x00000002 ; (1024 << x) = blocksize
+ENCODED_FARGMENT_SIZE:      dd 0x00000002 ; (1024 << x) = fragsize
+BLOCKS_PER_BLOCK_GROUP:     dd 0x00008000
+FRAGMENTS_PER_BLOCK_GROUP:  dd 0x00008000
+INODES_PER_BLOCK_GROUP:     dd 0x00006400
+LAST_MOUNT_TIME:            dd 0x00000000 ; POSIX
+LAST_WRITTEN_TIME:          dd 0x63B8EE47 ; POSIX
+
+UNVERIFIED_MOUNT_COUNT:     dw 0x0000
+UNVERIFIED_MOUNT_LIMIT:     dw 0xFFFF
+                            dw 0xEF53     ; EXT2 Verification Number
+FILE_SYSTEM_STATE:          dw 0x0001
+FILE_SYSTEM_ERROR_HANDLE:   dw 0x0001     ; do nothing
+VERSION_LOWER:              dw 0x0000
+
+LAST_VERIFICATION_TIME:     dd 0x63B8EE47
+VERIFICATION_INTERVAL:      dd 0x00000000
+CREATING_OS_ID:             dd 0x00000000 ; linux
+VERSION_UPPER:              dd 0x00000001
+
+RESERVED_BLOCK_UID:         dw 0x0000     ; user id
+RESERVED_BLOCK_GID:         dw 0x0000     ; group id
+; -------extended superblock fields--------
+FIRST_AVAILIABLE_INODE:     dd 0x0000000B
+
+SIZE_OF_INODE:              dw 0x0080
+SUPER_BLOCK_GROUP:          dw 0x0000
+
+EXTRA_FEATURES:             dd 0x00000038
+; dirs use hash index, fs auto_resize, inode extended attr
+REQUIRED_FEATURES:          dd 0x00000002
+; dirs have a type field
+READ_ONLY_FEATURES:         dd 0x00000003
+; sparse superblocks, 64 bit
+
+
+ext_superblock_end:
